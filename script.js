b98fa6643d110eb155001199744f4e5e0713f7a5
Zgibbs58 <113548748+Zgibbs58@users.noreply.github.com>
2023-04-23 18:42:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function changeBg(){
+    var navbar = document.getElementById('navbar');
+    var scrollValue = window.scrollY;
+    if(scrollValue > 75){
+        navbar.classList.add('navbarTransition');
+        
+    } else {
+        navbar.classList.remove('navbarTransition');
+        
+    }
+}
+window.addEventListener('scroll', changeBg);
+
+
+const navItem = document.getElementsByClassName('nav-link')
+
+for(var i = 0; i < navItem.length; i++) {
+  (function(index) {
+    navItem[index].addEventListener("click", function() {
+        setTimeout(function () {
+            closeBtn.click()
+        }, 500)
+     })
+  })(i);
+}
+
+lightbox.option({
+    "resizeDuration": 500,
+    "alwaysShowNavOnTouchDevices": true,
+    "showImageNumberLabel":true
+  });
+
+
